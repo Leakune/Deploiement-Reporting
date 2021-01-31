efc23ad1f88cebdabcad2d0e28faece885a00bd2
Ludovic FAVIER <lfavier@live.fr>
2021-01-31 18:31:05 +0100

    bug crashlytics et analytics

--- app/src/main/java/com/ludovic/psvsxbox/MainActivity.java
+++ app/src/main/java/com/ludovic/psvsxbox/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 import androidx.appcompat.app.AlertDialog;
@@ -20,22 +21,23 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
     private Button btn_PS, btn_XBOX, btn_exit;
     private FirebaseAnalytics mFirebaseAnalytics;
-
+    //private FirebaseCrashlytics​ crash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
         btn_PS = findViewById(R.id.btn_PS);
         btn_XBOX = findViewById(R.id.btn_XBOX);
         btn_exit = findViewById(R.id.btn_exit);
 
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+
         btn_PS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                eventAnalytics("btn_PS", "bouton ps", "bouton");
                 Intent i = new Intent(MainActivity.this, PS.class);
                 startActivity(i);
                 finish();
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         btn_XBOX.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                eventAnalytics("btn_XBOX", "bouton xbox", "bouton");
                 Intent i = new Intent(MainActivity.this, XBOX.class);
                 startActivity(i);
                 finish();
@@ -80,5 +83,12 @@ public class MainActivity extends AppCompatActivity {
         addContentView(crashButton, new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+    public void eventAnalytics(String id, String name, String type){
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, type);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
 }
